8931c82218d45eaa61ed3868cca39d18286486c8
DavidMorales <DavidMorales@172.16.220.211>
2017-11-27 07:54:20 -0500

    esta es la primera version sin internacionalizacion y es 2.0

--- Introduccion/src/introduccion/Principal.java
+++ Introduccion/src/introduccion/Principal.java
@@ -18,6 +18,7 @@ public class Principal {
         // TODO code application logic here
         
         System.out.println("Hola Mundo");
+        System.out.println("Hola");
         
         
     }
